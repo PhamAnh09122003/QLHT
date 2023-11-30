bbd4987630f7d59681d45044677ee70aabe6e4c4
Phạm Quỳnh Anh <134449773+PhamAnh09122003@users.noreply.github.com>
2023-11-30 16:11:07 +0700

    Thêm Class Function

---
+++ QLHT/Function.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLHT
+{
+    class Function
+    {
+        protected SqlConnection getConnection()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=DESKTOP-AB9KISM\\PHAMANH;Initial Catalog=Pharmacy;Integrated Security=True";
+
+            return con;
+        }
+        public DataSet getData(string query)
+        {
+            SqlConnection con = getConnection();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            //fire query
+            cmd.CommandText = query;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+        public void setData(string query, string msg)
+        {
+            SqlConnection con = getConnection();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show(msg, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public void datagetiing(string query)
+        {
+            SqlConnection con = getConnection();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
