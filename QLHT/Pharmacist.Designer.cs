606063f81d9ab362e1d69d526b2f7dc35bc0f12c
Phạm Quỳnh Anh <134449773+PhamAnh09122003@users.noreply.github.com>
2023-11-30 20:05:20 +0700

    Thêm giao diện quản lý của dược sĩ

---
+++ QLHT/Pharmacist.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace QLHT
+{
+    partial class Pharmacist
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pharmacist));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_LogOut = new Guna.UI2.WinForms.Guna2Button();
+            this.btnSellMedicine = new Guna.UI2.WinForms.Guna2Button();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.btnMediValidityCheck = new Guna.UI2.WinForms.Guna2Button();
+            this.btnModifyMedicine = new Guna.UI2.WinForms.Guna2Button();
+            this.btnViewMedicine = new Guna.UI2.WinForms.Guna2Button();
+            this.btnAddMedicine = new Guna.UI2.WinForms.Guna2Button();
+            this.btnDashboard = new Guna.UI2.WinForms.Guna2Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Location = new System.Drawing.Point(-1, -35);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1772, 1086);
+            this.panel2.TabIndex = 4;
+            // 
+            // btn_LogOut
+            // 
+            this.btn_LogOut.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btn_LogOut.CheckedState.FillColor = System.Drawing.Color.White;
+            this.btn_LogOut.CheckedState.ForeColor = System.Drawing.Color.Black;
+            this.btn_LogOut.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_LogOut.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_LogOut.ForeColor = System.Drawing.Color.White;
+            this.btn_LogOut.Image = ((System.Drawing.Image)(resources.GetObject("btn_LogOut.Image")));
+            this.btn_LogOut.Location = new System.Drawing.Point(3, 0);
+            this.btn_LogOut.Name = "btn_LogOut";
+            this.btn_LogOut.Size = new System.Drawing.Size(268, 45);
+            this.btn_LogOut.TabIndex = 10;
+            this.btn_LogOut.Text = "LogOut";
+            // 
+            // btnSellMedicine
+            // 
+            this.btnSellMedicine.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnSellMedicine.CheckedState.FillColor = System.Drawing.Color.White;
+            this.btnSellMedicine.CheckedState.ForeColor = System.Drawing.Color.Black;
+            this.btnSellMedicine.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnSellMedicine.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSellMedicine.ForeColor = System.Drawing.Color.White;
+            this.btnSellMedicine.Image = ((System.Drawing.Image)(resources.GetObject("btnSellMedicine.Image")));
+            this.btnSellMedicine.Location = new System.Drawing.Point(22, 758);
+            this.btnSellMedicine.Name = "btnSellMedicine";
+            this.btnSellMedicine.Size = new System.Drawing.Size(268, 45);
+            this.btnSellMedicine.TabIndex = 10;
+            this.btnSellMedicine.Text = "Bán thuốc";
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userNameLabel.ForeColor = System.Drawing.Color.White;
+            this.userNameLabel.Location = new System.Drawing.Point(96, 979);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(118, 40);
+            this.userNameLabel.TabIndex = 7;
+            this.userNameLabel.Text = "B-A-P";
+            this.userNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnMediValidityCheck
+            // 
+            this.btnMediValidityCheck.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnMediValidityCheck.CheckedState.FillColor = System.Drawing.Color.White;
+            this.btnMediValidityCheck.CheckedState.ForeColor = System.Drawing.Color.Black;
+            this.btnMediValidityCheck.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnMediValidityCheck.Font = new System.Drawing.Font("Century Gothic", 11F);
+            this.btnMediValidityCheck.ForeColor = System.Drawing.Color.White;
+            this.btnMediValidityCheck.Image = ((System.Drawing.Image)(resources.GetObject("btnMediValidityCheck.Image")));
+            this.btnMediValidityCheck.Location = new System.Drawing.Point(-7, 669);
+            this.btnMediValidityCheck.Name = "btnMediValidityCheck";
+            this.btnMediValidityCheck.Size = new System.Drawing.Size(317, 81);
+            this.btnMediValidityCheck.TabIndex = 6;
+            this.btnMediValidityCheck.Text = "Chứng nhận";
+            // 
+            // btnModifyMedicine
+            // 
+            this.btnModifyMedicine.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnModifyMedicine.CheckedState.FillColor = System.Drawing.Color.White;
+            this.btnModifyMedicine.CheckedState.ForeColor = System.Drawing.Color.Black;
+            this.btnModifyMedicine.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnModifyMedicine.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModifyMedicine.ForeColor = System.Drawing.Color.White;
+            this.btnModifyMedicine.Image = ((System.Drawing.Image)(resources.GetObject("btnModifyMedicine.Image")));
+            this.btnModifyMedicine.Location = new System.Drawing.Point(25, 604);
+            this.btnModifyMedicine.Name = "btnModifyMedicine";
+            this.btnModifyMedicine.Size = new System.Drawing.Size(268, 45);
+            this.btnModifyMedicine.TabIndex = 5;
+            this.btnModifyMedicine.Text = "Sửa đổi";
+            // 
+            // btnViewMedicine
+            // 
+            this.btnViewMedicine.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnViewMedicine.CheckedState.FillColor = System.Drawing.Color.White;
+            this.btnViewMedicine.CheckedState.ForeColor = System.Drawing.Color.Black;
+            this.btnViewMedicine.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnViewMedicine.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewMedicine.ForeColor = System.Drawing.Color.White;
+            this.btnViewMedicine.Image = ((System.Drawing.Image)(resources.GetObject("btnViewMedicine.Image")));
+            this.btnViewMedicine.Location = new System.Drawing.Point(25, 520);
+            this.btnViewMedicine.Name = "btnViewMedicine";
+            this.btnViewMedicine.Size = new System.Drawing.Size(268, 45);
+            this.btnViewMedicine.TabIndex = 4;
+            this.btnViewMedicine.Text = "Xem loại thuốc";
+            // 
+            // btnAddMedicine
+            // 
+            this.btnAddMedicine.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnAddMedicine.CheckedState.FillColor = System.Drawing.Color.White;
+            this.btnAddMedicine.CheckedState.ForeColor = System.Drawing.Color.Black;
+            this.btnAddMedicine.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnAddMedicine.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.btnAddMedicine.ForeColor = System.Drawing.Color.White;
+            this.btnAddMedicine.Image = ((System.Drawing.Image)(resources.GetObject("btnAddMedicine.Image")));
+            this.btnAddMedicine.Location = new System.Drawing.Point(25, 437);
+            this.btnAddMedicine.Name = "btnAddMedicine";
+            this.btnAddMedicine.Size = new System.Drawing.Size(268, 45);
+            this.btnAddMedicine.TabIndex = 3;
+            this.btnAddMedicine.Text = "Thêm loại thuốc";
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnDashboard.CheckedState.FillColor = System.Drawing.Color.White;
+            this.btnDashboard.CheckedState.ForeColor = System.Drawing.Color.Black;
+            this.btnDashboard.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnDashboard.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDashboard.ForeColor = System.Drawing.Color.White;
+            this.btnDashboard.Image = ((System.Drawing.Image)(resources.GetObject("btnDashboard.Image")));
+            this.btnDashboard.Location = new System.Drawing.Point(22, 367);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(268, 45);
+            this.btnDashboard.TabIndex = 2;
+            this.btnDashboard.Text = "Trang chủ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(31, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 59);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pharmacist";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(45, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 198);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.btn_LogOut);
+            this.panel1.Controls.Add(this.btnSellMedicine);
+            this.panel1.Controls.Add(this.userNameLabel);
+            this.panel1.Controls.Add(this.btnMediValidityCheck);
+            this.panel1.Controls.Add(this.btnModifyMedicine);
+            this.panel1.Controls.Add(this.btnViewMedicine);
+            this.panel1.Controls.Add(this.btnAddMedicine);
+            this.panel1.Controls.Add(this.btnDashboard);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 1107);
+            this.panel1.TabIndex = 1;
+            // 
+            // Pharmacist
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1621, 1030);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Pharmacist";
+            this.Text = "Pharmacist";
+            this.Load += new System.EventHandler(this.Pharmacist_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI2.WinForms.Guna2Button btn_LogOut;
+        private Guna.UI2.WinForms.Guna2Button btnSellMedicine;
+        private System.Windows.Forms.Label userNameLabel;
+        private Guna.UI2.WinForms.Guna2Button btnMediValidityCheck;
+        private Guna.UI2.WinForms.Guna2Button btnModifyMedicine;
+        private Guna.UI2.WinForms.Guna2Button btnViewMedicine;
+        private Guna.UI2.WinForms.Guna2Button btnAddMedicine;
+        private Guna.UI2.WinForms.Guna2Button btnDashboard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ QLHT/Pharmacist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLHT
+{
+    public partial class Pharmacist : Form
+    {
+        public Pharmacist()
+        {
+            InitializeComponent();
+        }
+
+        private void Pharmacist_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
